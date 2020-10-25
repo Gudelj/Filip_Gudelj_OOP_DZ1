5f1528362c2a6cfb608264e159543fbc15443337
Gudelj <72815114+Gudelj@users.noreply.github.com>
2020-10-25 16:48:27 +0100

    Add files via upload

---
+++ DZ1/DZ1/Episode.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DZ1
+{
+    public class Episode
+    {
+        private int viewers;
+        private double gradesSum;
+        private double maxGrade;
+        public static Random generator;
+
+        public Episode(int Viewers,double GradesSum, double MaxGrade)
+        {
+            viewers = Viewers;
+            gradesSum = GradesSum;
+            maxGrade = MaxGrade;
+        }
+        public Episode(): this(0,0,0) { }
+
+        public static double GenerateRandomScore()
+        {
+            generator = new Random();
+            double a;
+            return a = generator.NextDouble()*10;
+        }
+        public double GetMaxScore()
+        {
+            return maxGrade;
+        }
+        public void AddView(double a)
+        {
+            viewers++;
+            gradesSum+=a;
+            if (maxGrade < a)
+                maxGrade = a;
+        }
+        public double GetAverageScore()
+        {
+            return gradesSum / ((double)viewers);
+        }
+        public int GetViewerCount()
+        {
+            return viewers;
+        }
+     
+    }
+}
